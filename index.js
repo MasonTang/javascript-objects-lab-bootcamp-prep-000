891e85ebcc5ddc580398ccfa8f163fae6cdf788f
masontang@aol.com <masontang@aol.com>
2018-07-01 20:44:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
     return Object.assign({}, object, {[key]:value})
 }
 
+function deleteFromObjectByKey(object, key){
+    var newObj = Object.assign({}, object)
+    delete newObj[key]
+    return newObj
+}
