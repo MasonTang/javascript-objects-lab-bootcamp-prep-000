ed155c74b6bb2fb6ac38fc5025d11330dea8ff15
masontang@aol.com <masontang@aol.com>
2018-07-01 21:02:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var recipes = {}
+var recipes = {rice:4, milk:3}
 
 function updateObjectWithKeyAndValue(object, key, value) {
     return Object.assign({}, object, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    object[key] = value
+    return object
 }
 
 function deleteFromObjectByKey(object, key){
